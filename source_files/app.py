387d807d77fac123b6b5a1ea6a0377472bb99d0e
DevMhrn <debashismaharana7854@gmail.com>
2025-11-09 21:04:05 +0530

    Add Dockerfile and app.py for Python application deployment

---
+++ source_files/app.py
@@ -0,0 +1,9 @@
+print("Hello World from Docker!")
+print("This is a simple Python application running in a container.")
+print("DevOps Assignment - Docker Demonstration")
+
+# Keep the application running
+import time
+while True:
+    print("Application is running...")
+    time.sleep(10)
